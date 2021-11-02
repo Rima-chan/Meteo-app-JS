6a1818987666ab1ffe6d2db19f78add797d01d6c
Marie Beaujeu <marie.beaujeu@gmail.com>
2021-11-02 19:53:22 +0100

    First commit

---
+++ front/app.js
@@ -0,0 +1,43 @@
+const APIKEY = '07dfa3c159e050ea5e900f0f18be3b20';
+let form = document.querySelector("form");
+let searchInput;
+let searchOutput = [];
+
+const city = document.getElementById("result-1");
+const temp = document.getElementById("result-2");
+const humidity = document.getElementById("result-3");
+const wind = document.getElementById("result-4");
+const description = document.getElementById("weather-description");
+const icon = document.getElementById("icon");
+const iconUrl = ''
+
+
+function getValue() {
+    searchInput = document.getElementById("search-input").value;
+}
+
+async function apiCall(city) {
+    let host = getApiUrl();
+    let url = `${host}/weather/${city}`;
+    try {
+        const result = await fetch(url);
+        const data = await result.json();
+        console.log(result);
+        console.log(data)
+        document.getElementById("result-1").textContent = data.name;
+        temp.textContent = Math.ceil(data.main.temp) + " °C";
+        humidity.textContent = data.main.humidity + " %";
+        wind.textContent = Math.ceil(data.wind.speed*10) + " KM/H";
+        description.textContent = data.weather[0].description;
+        icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    apiCall(searchInput);
+})
+
+apiCall('paris');
---
+++ front/config.js
@@ -0,0 +1,4 @@
+function getApiUrl() {
+    let apiUrl = (location.hostname === "localhost"|| location.hostname === "127.0.0.1") ? "http://localhost:3000" : "https://meteo-app-js.herokuapp.com";
+    return apiUrl;
+}
